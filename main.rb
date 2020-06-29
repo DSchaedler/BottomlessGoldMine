844d4abcd1513c60a09fd56bcd0c96c2b2a0ba3a
D Schaedler <dominicschaedler@gmail.com>
2020-06-29 12:02:18 -0400

    Add files via upload

--- main.rb
+++ main.rb
@@ -1,7 +1,6 @@
 def tick args
   
   #Run Init
-  args.state.init_complete ||= false
   unless args.state.init_complete
     init(args)
   end
@@ -21,34 +20,32 @@ end
 
 def game_logic args
   
-  #runs every second
+  #Every Second
   if args.state.second_tick
-    if args.state.player_digs_value
+    if args.state.game_checkbox_dictionary[:auto_digs]
       args.state.total_gold_count += 1
     end
   end
   
-  #Handle Mouse Clicks - fires on Mouse Up
-  if args.inputs.mouse.up
-    click_location = [args.inputs.mouse.click.x, args.inputs.mouse.click.y, 1, 1]
-    puts click_location
+  #On Mouse Click
+  if args.inputs.mouse.click
+    #Store click location
+    click_location = [args.inputs.mouse.x, args.inputs.mouse.y, 1, 1]
     
-    if click_location.intersect_rect? args.state.player_digs_checkbox
-      args.state.player_digs_value = !args.state.player_digs_value
+    #Add Gold if mined manually
+    if click_location.intersect_rect? args.state.game_button_box[:mine_gold_button]
+      args.state.total_gold_count += 1
     end
-  end
-end
-
-=begin
-iter = 0
-    puts args.state.game_checkbox_visual[iter]
-    for i in args.state.game_checkbox_values
-      if click_location.intersect_rect? args.state.game_checkbox_visual[iter]
-        i = !i
-        iter +=1
+    
+    #Update checkboxes if clicked
+    args.state.game_checkbox_dictionary.each {|key, value| #Iterate through all checkboxes in container
+      if click_location.intersect_rect? args.state.game_checkbox_visual[key] #if the current checkbox was clicked
+        args.state.game_checkbox_dictionary[key] = !args.state.game_checkbox_dictionary[key] #flip the boolean value
       end
-    end
-=end
+    }
+  end
+  
+end
 
 def time args
   #reset clock booleans to 0
@@ -70,21 +67,26 @@ def time args
 end
 
 def init args
-  #Checkbox Values Container
-  args.state.game_checkbox_values ||= []
   
   #Define state trackers
-  args.state.total_gold_count ||= 0
-  args.state.player_digs_value ||= true
+  args.state.total_gold_count = 0
   
   #Define Time trackers
-  args.state.second_clock ||= 0
-  args.state.minute_clock ||= 0
+  args.state.second_clock = 0
+  args.state.minute_clock = 0
   
-  args.state.second_tick ||= false
-  args.state.minute_tick ||= false
+  args.state.second_tick = false
+  args.state.minute_tick = false
   
+  checkbox_init(args)
+  
+  #Mark Init Complete
   args.state.init_complete = true
+end
+
+def checkbox_init args
+  
+  args.state.game_checkbox_dictionary = {:auto_digs => false} #auto Digs
 end
 
 def define_ui args
@@ -92,56 +94,57 @@ def define_ui args
   #Define margins and element sizes
   
   checkbox_size = 20
-  
   left_checkbox_margin = 10
+  
   left_label_margin = left_checkbox_margin + checkbox_size + 10
+  
+  button_text_margin = 10
+  
   top_margin = 10
   line_spacing = 25
   
   
-  #Create UI Elements
-  
   #Label Container
   args.state.game_labels = []
-  
-  #Checkbox Visuals Container
-  args.state.game_checkbox_visual = []
   
   #total_gold_count label
   args.state.total_gold_count_label = [left_label_margin, args.grid.top - top_margin, "Total Gold Reserves: #{args.state.total_gold_count}"]
   args.state.game_labels << args.state.total_gold_count_label
   
-  #Increment top margin to allow for spacing
   top_margin += line_spacing
   
-  #player_digs checkbox
-  args.state.player_digs_checkbox = [left_checkbox_margin, args.grid.top - top_margin - checkbox_size, checkbox_size, checkbox_size, 0, 0, 0]
-  args.state.game_checkbox_values << args.state.player_digs_value
-  args.state.game_checkbox_visual << args.state.player_digs_checkbox
   
-  #player_digs label
-  args.state.player_digs_label = [left_label_margin, args.grid.top - top_margin, "Player Digs?: #{args.state.player_digs_value}"]
-  args.state.game_labels << args.state.player_digs_label
+  #auto_digs checkbox and label
+  args.state.game_checkbox_visual = {:auto_digs => [left_checkbox_margin, args.grid.top - top_margin - checkbox_size, checkbox_size, checkbox_size, 0, 0, 0]}
+  args.state.game_labels << [left_label_margin, args.grid.top - top_margin, "Auto Dig?: #{args.state.game_checkbox_dictionary[:auto_digs]}"]
   
-  #Increment top margin to allow for spacing
   top_margin += line_spacing
+  
+  # Mine Gold button
+  args.state.game_button_text = {:mine_gold_button => "Mine Gold"}
+  mine_gold_text_size = args.gtk.calcstringbox(args.state.game_button_text[:mine_gold_button])
+  args.state.game_button_box = {:mine_gold_button => [left_label_margin, args.grid.top - top_margin - mine_gold_text_size[1] - ( button_text_margin * 2), mine_gold_text_size[0] + (button_text_margin*2), mine_gold_text_size[1] + ( button_text_margin * 2)]}
+  args.state.game_labels << [left_label_margin + button_text_margin, args.grid.top - top_margin - button_text_margin, args.state.game_button_text[:mine_gold_button]]
+  
 end
 
 def draw_ui args
   #Draw Game Labels
-  for i in args.state.game_labels
-    args.outputs.labels << i
-  end
+  args.state.game_labels.each {|label|
+    args.outputs.labels << label
+  }
+  
+  #Draw Game Buttons
+  args.state.game_button_box.each {|key, box|
+    args.outputs.borders << box
+  }
   
   #Determine Checkbox State, draw as appropriate
-  iter = 0
-  for i in args.state.game_checkbox_values
-    if i #if checked
-      args.outputs.solids << args.state.game_checkbox_visual[iter]
-      iter += 1
+  args.state.game_checkbox_dictionary.each {|key, value| #check all values in the check values container
+    if value #if checked
+      args.outputs.solids << args.state.game_checkbox_visual[key] #filled
     else
-      args.outputs.borders << args.state.game_checkbox_visual[iter]
-      iter += 1
+      args.outputs.borders << args.state.game_checkbox_visual[key] #empty
     end
-  end
+  }
 end
