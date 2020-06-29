fedf2bf9f9610fe90a8a709593028b61bbc7203a
D Schaedler <dominicschaedler@gmail.com>
2020-06-29 13:24:03 -0400

    Add files via upload

--- main.rb
+++ main.rb
@@ -19,11 +19,20 @@ def tick args
 end
 
 def game_logic args
+
+  if args.state.pickaxe_durability > 0
+    can_dig = true
+  else
+    can_dig = false
+  end
   
   #Every Second
   if args.state.second_tick
-    if args.state.game_checkbox_dictionary[:auto_digs]
-      args.state.total_gold_count += 1
+  
+    #Auto Dig
+    if args.state.game_checkbox_dictionary[:auto_dig] && can_dig
+      args.state.total_gold_count += args.state.auto_dig_income
+      args.state.pickaxe_durability -= args.state.auto_dig_wear
     end
   end
   
@@ -33,8 +42,15 @@ def game_logic args
     click_location = [args.inputs.mouse.x, args.inputs.mouse.y, 1, 1]
     
     #Add Gold if mined manually
-    if click_location.intersect_rect? args.state.game_button_box[:mine_gold_button]
+    if (click_location.intersect_rect? args.state.game_button_box[:mine_gold]) && can_dig
       args.state.total_gold_count += 1
+      args.state.pickaxe_durability -= 1
+    end
+    
+    #Buy more pickaxe_durability
+    if ((click_location.intersect_rect? args.state.game_button_box[:buy_pickaxe]) && (args.state.total_gold_count >= args.state.pickaxe_price))
+      args.state.total_gold_count -= args.state.pickaxe_price
+      args.state.pickaxe_durability += args.state.pickaxe_durability
     end
     
     #Update checkboxes if clicked
@@ -45,48 +61,6 @@ def game_logic args
     }
   end
   
-end
-
-def time args
-  #reset clock booleans to 0
-  args.state.second_tick = false
-  args.state.minute_tick = false
-  
-  #Once 1 Second (60 frames) has passed, increment the second_clock, put out a second_tick, and reset the frame_clock
-  if args.state.tick_count % 60 == 0
-    args.state.second_clock += 1
-    args.state.second_tick = true
-  end
-  
-  if args.state.second_clock >= 60
-    args.state.minute_clock += 1
-    args.state.minute_tick = true
-    args.state.second_clock = 0
-  end
-  
-end
-
-def init args
-  
-  #Define state trackers
-  args.state.total_gold_count = 0
-  
-  #Define Time trackers
-  args.state.second_clock = 0
-  args.state.minute_clock = 0
-  
-  args.state.second_tick = false
-  args.state.minute_tick = false
-  
-  checkbox_init(args)
-  
-  #Mark Init Complete
-  args.state.init_complete = true
-end
-
-def checkbox_init args
-  
-  args.state.game_checkbox_dictionary = {:auto_digs => false} #auto Digs
 end
 
 def define_ui args
@@ -106,25 +80,39 @@ def define_ui args
   
   #Label Container
   args.state.game_labels = []
+  args.state.game_button_text = Hash.new
+  args.state.game_button_box = Hash.new
   
   #total_gold_count label
-  args.state.total_gold_count_label = [left_label_margin, args.grid.top - top_margin, "Total Gold Reserves: #{args.state.total_gold_count}"]
-  args.state.game_labels << args.state.total_gold_count_label
-  
+  args.state.game_labels <<  [left_label_margin, args.grid.top - top_margin, "Gold: #{args.state.total_gold_count}"]
   top_margin += line_spacing
   
+  # pickaxes label
+  pickaxes = (args.state.pickaxe_durability / 20).ceil
+  args.state.game_labels << [left_label_margin, args.grid.top - top_margin, "Pickaxes: #{pickaxes} (#{args.state.pickaxe_durability} durability)"]
   
-  #auto_digs checkbox and label
-  args.state.game_checkbox_visual = {:auto_digs => [left_checkbox_margin, args.grid.top - top_margin - checkbox_size, checkbox_size, checkbox_size, 0, 0, 0]}
-  args.state.game_labels << [left_label_margin, args.grid.top - top_margin, "Auto Dig?: #{args.state.game_checkbox_dictionary[:auto_digs]}"]
+  top_margin += line_spacing
+  top_margin += line_spacing
+  
+  #auto_dig checkbox and label
+  args.state.game_checkbox_visual = {:auto_dig => [left_checkbox_margin, args.grid.top - top_margin - checkbox_size, checkbox_size, checkbox_size, 0, 0, 0]}
+  args.state.game_labels << [left_label_margin, args.grid.top - top_margin, "Auto Dig?: - #{args.state.auto_dig_wear} durability"]
   
   top_margin += line_spacing
   
   # Mine Gold button
-  args.state.game_button_text = {:mine_gold_button => "Mine Gold"}
-  mine_gold_text_size = args.gtk.calcstringbox(args.state.game_button_text[:mine_gold_button])
-  args.state.game_button_box = {:mine_gold_button => [left_label_margin, args.grid.top - top_margin - mine_gold_text_size[1] - ( button_text_margin * 2), mine_gold_text_size[0] + (button_text_margin*2), mine_gold_text_size[1] + ( button_text_margin * 2)]}
-  args.state.game_labels << [left_label_margin + button_text_margin, args.grid.top - top_margin - button_text_margin, args.state.game_button_text[:mine_gold_button]]
+  args.state.game_button_text[:mine_gold] = "Mine Gold: +1 Gold"
+  mine_gold_text_size = args.gtk.calcstringbox(args.state.game_button_text[:mine_gold])
+  args.state.game_button_box[:mine_gold] = [left_label_margin, args.grid.top - top_margin - mine_gold_text_size[1] - ( button_text_margin * 2), mine_gold_text_size[0] + (button_text_margin*2), mine_gold_text_size[1] + ( button_text_margin * 2)]
+  args.state.game_labels << [left_label_margin + button_text_margin, args.grid.top - top_margin - button_text_margin, args.state.game_button_text[:mine_gold]]
+  
+  top_margin += line_spacing + mine_gold_text_size[1]
+  
+  # Buy Pickaxe button
+  args.state.game_button_text[:buy_pickaxe] = "Buy Pickaxe: -5 Gold"
+  buy_pickaxe_text_size = args.gtk.calcstringbox(args.state.game_button_text[:buy_pickaxe])
+  args.state.game_button_box[:buy_pickaxe] = [left_label_margin, args.grid.top - top_margin - buy_pickaxe_text_size[1] - ( button_text_margin * 2), buy_pickaxe_text_size[0] + (button_text_margin*2), buy_pickaxe_text_size[1] + ( button_text_margin * 2)]
+  args.state.game_labels << [left_label_margin + button_text_margin, args.grid.top - top_margin - button_text_margin, args.state.game_button_text[:buy_pickaxe]]
   
 end
 
@@ -147,4 +135,61 @@ def draw_ui args
       args.outputs.borders << args.state.game_checkbox_visual[key] #empty
     end
   }
+end
+
+def init args
+  
+  #Define state trackers
+  
+  #Gold
+  args.state.total_gold_count = 0
+  
+  #Pickaxes
+  args.state.pickaxe_durability = 20
+  args.state.pickaxe_price = 5
+  args.state.pickaxe_durability = 20
+  
+  #Atuo Dig
+  args.state.auto_dig_income = 1
+  args.state.auto_dig_wear = 2
+  
+  #Time
+  args.state.second_clock = 0
+  args.state.minute_clock = 0
+  
+  args.state.second_tick = false
+  args.state.minute_tick = false
+  
+  checkbox_init(args)
+  
+  #Mark Init Complete
+  args.state.init_complete = true
+end
+
+def checkbox_init args
+  #Default Checkbox values
+  args.state.game_checkbox_dictionary = {:auto_dig => false} #auto Digs
+end
+
+def time args
+  #reset clock booleans to 0
+  args.state.second_tick = false
+  args.state.minute_tick = false
+  
+  #Once 1 Second (60 frames) has passed, increment the second_clock, put out a second_tick, and reset the frame_clock
+  if args.state.tick_count % 60 == 0
+    args.state.second_clock += 1
+    args.state.second_tick = true
+  end
+  
+  if args.state.second_clock >= 60
+    args.state.minute_clock += 1
+    args.state.minute_tick = true
+    args.state.second_clock = 0
+  end
+  
+end
+
+def reset
+  $gtk.reset()
 end
